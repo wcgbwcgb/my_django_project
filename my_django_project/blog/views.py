6e1953b74c1faa785b6bae5407f7be9f93370ffb
Muyang <977155494@qq.com>
2025-08-22 17:34:34 -0400

    添加了html和css文件，设置了url

--- my_django_project/blog/views.py
+++ my_django_project/blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+post = [
+    {
+        'author':"Hanson Han",
+        "title": "Blog post 1",
+        "content":"Hello",
+        "date_posted":"August 27, 2018"
+    },
+    {
+        'author':"Liam Xu",
+        "title": "Blog post 2",
+        "content":"Hi",
+        "date_posted":"August 30, 2018"
+    }
+]
+
+def home(request):
+    context = {
+        "posts":post
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':"About"})
